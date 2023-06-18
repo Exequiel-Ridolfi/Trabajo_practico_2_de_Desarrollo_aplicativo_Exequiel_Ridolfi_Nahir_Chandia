2bc0db964f1267016d867c57a10edf808611839b
exe <ridolfiexe17@gmail.com>
2023-06-17 23:03:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trabajo_Practico_2_D_A_Exequiel_Ridolfi_Nahir_Chandia',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
