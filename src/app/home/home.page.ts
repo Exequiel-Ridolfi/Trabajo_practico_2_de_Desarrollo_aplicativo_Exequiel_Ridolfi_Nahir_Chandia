ffa64276e17a93d17284284ba2e382207edfbc17
NAHIRCH2 <nahirchandia@gmail.com>
2023-06-23 15:51:25 -0300

    version 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,36 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServicioService } from '../services/service.service';
 import { Result } from '../interfaces/interfaces'; 
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  arraypokemon:Result[]=[]
-  constructor(private service:ServicioService, private _router:Router) {}
-  ngOnInit(){
-    this.service.pokedex().subscribe(respuesta=>{
-      console.log(respuesta)
-      this.arraypokemon=respuesta.results
-     })
-  }
-  gotoinfo(){
-   this._router.navigate(['tabs/masinfo'])
+  textobuscar = '';
+  arraypokemon: Result[] = [];
+  pokemonsFiltrados: Result[] = [];
+
+  constructor(private service: ServicioService, private router: Router) {}
+
+  ngOnInit() {
+    this.service.pokedex().subscribe(respuesta => {
+      console.log(respuesta);
+      this.arraypokemon = respuesta.results;
+      this.pokemonsFiltrados = this.arraypokemon;
+    });
   }
 
+  buscar() {
+    const texto = this.textobuscar.toLowerCase();
+    this.pokemonsFiltrados = this.arraypokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(texto);
+    });
   }
+
+  gotoinfo() {
+    this.router.navigate(['tabs/masinfo']);
+  }
+}
